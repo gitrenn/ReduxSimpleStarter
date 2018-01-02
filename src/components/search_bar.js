468c1a9a28bb318e5316a27a6161d0935f088ab4
Ren <rjingmembers@gmail.com>
2018-01-02 11:08:18 -0800

    created functions to search youtube videos via youtube api

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,9 +2,24 @@ import React, {Component} from 'react';
 
 // Create a new search bar component 
 class SearchBar extends Component {
-    render() {
-        return <input />
+    constructor(props) {
+        super(props);
+
+        this.state = { term: ''};// initialize a new state object and assign it to this.state
     }
+
+    render() {
+        return (
+            <div>
+                <input 
+                value={this.state.term} // controlled components
+                onChange={event => this.setState({ term: event.target.value})} />
+            </div>
+        );
+    }
+
+
+     
 }
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyBlsDKSEnADH6MFN6rhFZQNfJuN-ceK99k';
 
+
 // create a new component 
-const App = () => {
-    return <div>
-        <SearchBar />
-    </div>
+class App extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = { videos: []};
+
+        YTSearch({key: API_KEY, term: 'gift giving'}, videos => {
+            this.setState({videos});
+        })
+    }
+    render(){
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        )
+    }
 }
+    
+
 
 // render the component on the page we want
 ReactDOM.render(<App />, document.querySelector('.container'));
