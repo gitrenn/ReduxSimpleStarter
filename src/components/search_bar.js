93fa44d606d4f568bb926282866145ca2b104294
Ren <rjingmembers@gmail.com>
2018-01-01 16:59:57 -0800

    created a new component search bar and rendered it on homepage

---
+++ src/components/search_bar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+// Create a new search bar component 
+const SearchBar = () => {
+    return <input />
+};
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyBlsDKSEnADH6MFN6rhFZQNfJuN-ceK99k';
 
 // create a new component 
 const App = () => {
-    return <div>Hi!</div>
+    return <div>
+        <SearchBar />
+    </div>
 }
 
 // render the component on the page we want
