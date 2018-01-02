d61f483c9ab9ea269865bd998180729005614545
Ren <rjingmembers@gmail.com>
2018-01-02 15:57:04 -0800

    added lodash package to set timeout to render list when typing in search box

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Grid, Card, Icon, Loader } from 'semantic-ui-react';
+import { Embed, Grid, Card, Icon, Loader } from 'semantic-ui-react';
 
 const VideoDetail = ({ video }) => {
     if (!video) {
         return <Loader active inline />;
     }
     const title = video.snippet.title;
+    const imageUrl = video.snippet.thumbnails.high.url;
     const description = video.snippet.description;
     const videoId = video.id.videoId;
     const url = `https://www.youtube.com/embed/${videoId}`;
@@ -13,26 +14,19 @@ const VideoDetail = ({ video }) => {
     return (
         <Grid>
             <Grid.Column width={9}>
-                <Card className="card-custom">
-                    <iframe className="" src={url}></iframe>
+            <Embed className="video-custom"
+                id={videoId}
+                placeholder = {imageUrl}
+                source='youtube'
+            />
+            <Card className="card-custom">
                     <Card.Content>
                         <Card.Header>
                             {title}
                         </Card.Header>
-                        <Card.Meta>
-                            <span className='date'>
-                                Joined in 2015
-                    </span>
-                        </Card.Meta>
                         <Card.Description>
                             {description}
                         </Card.Description>
-                    </Card.Content>
-                    <Card.Content extra>
-                        <a>
-                            <Icon name='heart' />
-                            22 Likes
-                </a>
                     </Card.Content>
                 </Card>
             </Grid.Column>
